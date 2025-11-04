d6bb1b621f8ef8d799aaf1916e2cec7b26d00742
Samy ZANOUDA <samzan025@gmail.com>
2025-11-04 11:51:07 +0100

    Authentification par OIDC OK jusqu'au proxy vers serveur cyrus

--- Docker/Roundcube/config/config.inc.php
+++ Docker/Roundcube/config/config.inc.php
@@ -16,14 +16,14 @@
     $config['oauth_provider'] = 'generic';
     $config['oauth_provider_name'] = 'Keycloak';
     $config['oauth_client_id'] = 'roundcube';
-    $config['oauth_client_secret'] = 'kt2V6lVxVG49HVXQVXC2o1Mqjp5uplTA';
-    $config['oauth_redirect_uri'] = 'http://roundcube.localhost:20080/?_task=login&_action=oauth_callback';
-    $config['oauth_post_logout_redirect'] = 'http://roundcube.localhost:20080/';
-    $config['oauth_auth_uri'] = 'https://keycloak.localhost:38443/realms/example.com/protocol/openid-connect/auth';
-    $config['oauth_token_uri'] = 'https://keycloak.localhost:38443/realms/example.com/protocol/openid-connect/token';
-    $config['oauth_identity_uri'] = 'https://keycloak.localhost:38443/realms/example.com/protocol/openid-connect/userinfo';
+    $config['oauth_client_secret'] = 'S9CC8kyju55sUvIv6g1zaa30eNPyjx0k';
+    $config['oauth_redirect_uri'] = 'http://roundcube.example.com:20080/?_task=login&_action=oauth_callback';
+    $config['oauth_post_logout_redirect'] = 'http://roundcube.example.com:20080/';
+    $config['oauth_auth_uri'] = 'https://keycloak.example.com:8443/realms/example.com/protocol/openid-connect/auth';
+    $config['oauth_token_uri'] = 'https://keycloak.example.com:8443/realms/example.com/protocol/openid-connect/token';
+    $config['oauth_identity_uri'] = 'https://keycloak.example.com:8443/realms/example.com/protocol/openid-connect/userinfo';
     $config['oauth_scope'] = 'openid profile email';
-    $config['oauth_identity_fields'] = ['uid'];  
+    $config['oauth_identity_fields'] = ['email'];  
     
     // $config['oauth_login_redirect'] = true;
     // Permet de rediriger directement vers la page de login OAuth
